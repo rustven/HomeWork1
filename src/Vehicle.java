bd7258eb9daaa5772f825626686e198e191c79e7
Andrej Rusetski <rustven@gmail.com>
2023-12-13 21:50:51 +0100

    creat head class Vehicle

---
+++ src/Vehicle.java
@@ -0,0 +1,10 @@
+public class Vehicle {
+
+    public String brand;
+   public int year;
+
+    public Vehicle(String brand, int year){
+        this.brand = brand;
+        this.year = year;
+    }
+}
