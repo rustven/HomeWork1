7c5f77b16c57dc7d7d063109af3867fc1636a736
Andrej Rusetski <rustven@gmail.com>
2023-12-13 22:02:02 +0100

    add Car class

---
+++ src/Car.java
@@ -0,0 +1,9 @@
+public class Car extends Vehicle{
+
+    public String model;
+
+    public Car(String brand, int year, String model){
+        super(brand,year);
+        this.model = model;
+    }
+}
